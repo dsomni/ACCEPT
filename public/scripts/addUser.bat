79f609a7d28a709c0724ab89c57aa12b3b29a7d4
dsomni <pro100pro10010@gmail.com>
2020-12-29 18:27:45 +0300

    addUser script added

---
+++ public/scripts/addUser.bat
@@ -0,0 +1,1 @@
+node addUser.js
--- public/scripts/addUser.js
+++ public/scripts/addUser.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const config = require('../../config/db');
 
 //MongoDB connecting  
 mongoose.connect(config.db,{
@@ -12,18 +13,55 @@ mongoose.connection.on('error', (err) => {
     console.log("Error while connecting to DB: "+ err);
 });
 
+mongoose.set('useCreateIndex', true);
+
 
 var UserSchema = new mongoose.Schema({
-    login: String,
-    password: String
+    login: { 
+        type: String, 
+        unique: true,
+        index: true
+    },
+    password: String,
+    name : String,
+
+    grade: String,
+    attempts: Object,
+
+    isTeacher: Boolean,
+    hasClasses: Array
 });
 
 // Create model from schema
 var User = mongoose.model('User', UserSchema );
 
-function addUser (login,password){
+function addStudent (login, password, name, grade){
     User.insertMany([{
         login: login,
-        password: password
-    }])
+        password: password,
+        name: name,
+
+        grade: grade,
+        attempts: {},
+
+        isTeacher : false
+    }]);
 }
+
+async function addTeacher (login, password, name, hasClasses){
+    User.insertMany([{
+        login: login,
+        password: password,
+        name: name,
+
+        isTeacher: true,
+        hasClasses: hasClasses
+    }]);
+}
+function toDo(){
+    addStudent('96','1', 'Dima', '12A')
+    addStudent('96','1', 'LzheDima', '12B')
+    addTeacher('0','0', 'admin', ['12A', '12B'])
+}
+
+toDo()
