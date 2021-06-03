dcc396c6d1ee4adb5768694931c0b52e793e7613
dsomni <pro100pro10010@gmail.com>
2021-06-03 20:43:26 +0300

    Update [rating permissions] rating table

--- config/models/Quiz.js
+++ config/models/Quiz.js
@@ -2,13 +2,9 @@ const mongoose = require('mongoose')
 const config = require('../configs');
 
 const QuizSchema = new mongoose.Schema({
-  identificator: Number,//Quiz odd if template else even
+  identificator: Number,
   title: String,
   description: String,
-  parent: Number,
-  teacher: String,
-  template: Boolean,//When start quiz create copy
-  grade: String,
   tasks: [{
     identificator: String,
     author: String,
@@ -21,33 +17,34 @@ const QuizSchema = new mongoose.Schema({
   }],
   author: String,
   duration: Number,
-  whenEnds: String,//Date.now()+duration on start quiz
-  isBegan: Boolean,
-  isEnded: Boolean,
-  results: [{
-    login: String,
-    sumscore: Number,
-    tasks: [{
-      score: Number,
-      tries: Number,
-      attempts: [{
-        date: String,
-        score: String
+  hasActiveLesson: Boolean,
+  lessons:[{
+    grade: String,
+    whenEnds: String,
+    teacher:String,
+    results: [{
+      login: String,
+      sumscore: Number,
+      tasks: [{
+        score: Number,
+        tries: Number,
+        attempts: [{
+          date: String,
+          score: String
+        }]
       }]
-    }]
-  }],
-  attempts: [{//do not add attempts to user.attempts but add here
-    login: String,
-    date: String,
-    TaskID: String,
-    score: Number,
-    programText: String,
-    language: String,
-    result: [String]
+    }],
+    attempts: [{
+      login: String,
+      date: String,
+      TaskID: String,
+      score: Number,
+      programText: String,
+      language: String,
+      result: [String]
+    }],
   }]
-
 }, { collection: config.mongodbConfigs.CollectionNames.quizes });
 
 // Create model from schema
 module.exports = Quiz = mongoose.model('Quiz', QuizSchema);
-
